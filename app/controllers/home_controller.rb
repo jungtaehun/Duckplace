451ef95f45930edf551761ce37c222c877f90871
bada <wxg1297@naver.com>
2016-06-18 18:32:07 +0900

    naver geocode api success

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,7 @@
+require 'net/http'
+require 'nokogiri'
+require "json"
+
 class HomeController < ApplicationController
   def index
     
@@ -14,10 +18,24 @@ class HomeController < ApplicationController
     duck.region = params[:region]
     duck.mtype = params[:type]
     duck.name = params[:name]
-    duck.address = params[:address]
     duck.daddress = params[:daddress]
-    duck.mlong= 127.017784
-    duck.mlat = 37.484709
+
+
+    uri = URI(URI.encode("https://openapi.naver.com/v1/map/geocode?query=#{params[:address]}"))
+
+    req = Net::HTTP::Get.new(uri)
+    req['X-Naver-Client-Id'] = "DZaaw4YVOevidOL4rL06"
+    req['X-Naver-Client-Secret'] = "AgZIyTdxqI"
+
+    res = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == 'https') {|http|
+      http.request(req)}
+
+    parsed = JSON.parse(res.body)["result"]["items"][0]
+    duck.address = parsed["address"]
+    duck.mlong = parsed["point"]["x"]
+    duck.mlat = parsed["point"]["y"]
+
+
     uploader = DuckUploader.new
     uploader.store!(params[:pic])
 
